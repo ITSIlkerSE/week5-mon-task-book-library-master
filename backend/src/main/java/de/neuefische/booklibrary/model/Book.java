9a09e14d2924dee8ccfdffc2057d42ca8fe7ab0b
Ilker Yildirim <ITSIlkerSE@gmail.com>
2022-10-07 15:14:07 +0200

    Add backend

---
+++ backend/src/main/java/de/neuefische/booklibrary/model/Book.java
@@ -0,0 +1,22 @@
+package de.neuefische.booklibrary.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "books")
+public class Book {
+
+    @Id
+    private String isbn;
+
+    private String title;
+    private String author;
+
+}
---
+++ backend/src/main/java/de/neuefische/booklibrary/repository/BookRepository.java
@@ -0,0 +1,54 @@
+
+input{
+  text-align: center;
+}
+
+svg{
+  position: relative;
+  fill: #704513;
+}
+video{
+  object-fit: cover;
+}
+.Box{
+  position: absolute;
+  border-radius: 2px;
+  left: 13px;
+  top: 10px;
+  content: " ";
+  background-color: whitesmoke;
+  width: 76.3px;
+  height:89.5px;
+
+}
+.App-header__title{
+  font-size: 1rem;
+  color: white;
+  display: flex;
+gap: 1em;
+  position: relative;
+}
+
+.App {
+  text-align: center;
+}
+
+
+.App-logo {
+  height: 40vmin;
+  pointer-events: none;
+}
+
+@media (prefers-reduced-motion: no-preference) {
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+  }
+}
+
+.App-header {
+  display: flex;
+  align-items: center;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  font-size: calc(10px + 2v
